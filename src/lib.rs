fc860be03ee682a493357bf6624d70c19f8e556c
Sven-Hendrik Haase <sh@lutzhaase.com>
2016-04-30 13:39:41 +0200

    Add font stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
 extern crate sdl2;
+extern crate sdl2_ttf;
+extern crate rand;
+
+use std::path::Path;
+use std::thread;
+use std::time::Duration;
 
 use sdl2::pixels::Color;
 use sdl2::event::Event::*;
+use sdl2::rect::Rect;
 use sdl2::keyboard::Keycode::*;
-use std::thread;
-use std::time::Duration;
+use sdl2::render::TextureQuery;
+use rand::{Rng, Rand};
+use rand::distributions::{IndependentSample, Range};
 
 mod state;
 
@@ -37,10 +45,19 @@ impl<'e> Game<'e> {
     }
 
     pub fn run(&mut self) {
+        let screen_width = 800;
+        let screen_height = 600;
+
+        let mut rng = rand::thread_rng();
         let sdl_context = sdl2::init().unwrap();
         let mut timer = sdl_context.timer().unwrap();
         let mut event_pump = sdl_context.event_pump().unwrap();
         let video = sdl_context.video().unwrap();
+        let ttf_context = sdl2_ttf::init().unwrap();
+
+        let mut font = ttf_context.load_font(Path::new("resources/DejaVuSerif.ttf"), 128).unwrap();
+        let surface = font.render("ruffel")
+            .blended(Color::rand(&mut rng)).unwrap();
 
         let window = video.window("Ruffel", 800, 600)
             .position_centered().opengl()
@@ -50,8 +67,12 @@ impl<'e> Game<'e> {
             .accelerated()
             .build().unwrap();
 
-        let mut done = false;
-        let mut delta = Duration::new(0, 0);
+        let mut font_texture = renderer.create_texture_from_surface(&surface).unwrap();
+
+        let TextureQuery { width, height, .. } = font_texture.query();
+
+        // If the example text is too big for the screen, downscale it (and center irregardless)
+        let padding = 64;
 
         // Initialize State handlers
         for s in &mut self.states
@@ -59,11 +80,29 @@ impl<'e> Game<'e> {
             s.init();
         }
 
+        let mut done = false;
+        let mut delta = Duration::new(0, 0);
         while !done {
             let start_time = timer.ticks();
-            renderer.set_draw_color(Color::RGB(0, 0, 0));
+
+            for event in event_pump.poll_iter() {
+                match event {
+                    Quit { .. } => done = true,
+                    KeyDown { keycode, .. } => match keycode {
+                        Some(Escape) => done = true,
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            }
+
+            let between = Range::new(0, 400);
+            let target = Rect::new(between.ind_sample(&mut rng), 50, between.ind_sample(&mut rng) as u32, 500);
+            renderer.set_draw_color(Color::rand(&mut rng));
             renderer.clear();
+            renderer.copy(&mut font_texture, None, Some(target));
             renderer.present();
+
 
             // Updating
             for s in &mut self.states
@@ -77,22 +116,9 @@ impl<'e> Game<'e> {
                 s.draw();
             }
 
-
-            thread::sleep_ms(1000/60);
-            for event in event_pump.poll_iter() {
-                match event {
-                    Quit { .. } => done = true,
-                    KeyDown { keycode, .. } => match keycode {
-                        Some(Escape) => done = true,
-                        _ => {}
-                    },
-                    _ => {}
-                }
-            }
-
             let end_time = timer.ticks();
             delta = Duration::from_millis((end_time - start_time) as u64);
-            println!("{:?}", delta);
+            thread::sleep_ms(1000/60);
         }
     }
 }
