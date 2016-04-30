fddce9afd2ad161842beb677209aa4eb0ae56dd0
Alexander Dmitriev <alexander.dmitriev2580@gmail.com>
2016-04-30 12:30:13 +0200

    game loop stub

--- examples/game.rs
+++ examples/game.rs
@@ -1,7 +1,36 @@
 extern crate ggez;
 
-use ggez::ralf;
+//use ggez::Engine;
+//use ggez::State;
+use ggez::*;
+
+struct Game
+{
+    i: i32
+}
+
+impl State for Game
+{
+    fn init(&self) -> Result<(), GameError>
+    {
+        print!("init");
+        Ok(())
+    }
+    fn update(&self) -> Result<(), GameError>
+    {
+        print!("update");
+        Ok(())
+    }
+    fn draw(&self) -> Result<(), GameError>
+    {
+        print!("draw");
+        Ok(())
+    }
+}
 
 pub fn main() {
-    ralf();
+    let mut g: Game = Game { i:5 };
+    let mut e: Engine = Engine::new();
+    e.add_obj(&mut g);
+    e.ralf();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,50 @@
-pub fn ralf() {
-    println!("ralf");
+
+
+pub enum GameError {
+    Lolwtf
+}
+
+pub trait State {
+    fn init(&self) -> Result<(), GameError>;
+    fn update(&self) -> Result<(), GameError>;
+    fn draw(&self) -> Result<(), GameError>;
+}
+
+pub struct Engine<'e>
+{
+    states: Vec<&'e mut State>
+}
+
+impl<'e> Engine<'e> {
+    pub fn new() -> Engine<'e>
+    {
+        Engine
+        {
+            states: Vec::new()
+        }
+    }
+
+    pub fn add_obj(&mut self, s: &'e mut State)
+    {
+        self.states.push(s)
+    }
+
+    pub fn ralf(&mut self)
+    {
+        for s in &mut self.states
+        {
+            s.init();
+        }
+        loop
+        {
+            for s in &mut self.states
+            {
+                s.update();
+            }
+            for s in &mut self.states
+            {
+                s.draw();
+            }
+        }
+    }
 }
