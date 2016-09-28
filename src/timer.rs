a9c8679126f2344aadfd3fc64616e8cc26937d9f
Simon Heath <icefoxen@gmail.com>
2016-09-27 21:54:29 -0400

    Starting to flesh out timer library

---
+++ src/timer.rs
@@ -0,0 +1,41 @@
+
+
+fn get_average_delta() {
+}
+
+fn get_delta() {
+}
+
+fn get_fps() {
+}
+
+fn get_time() {
+}
+
+fn sleep() {
+}
+
+
+
+
+// function love.load()
+//     min_dt = 1/30
+//     next_time = love.timer.getTime()
+//     end
+
+//     function love.update(dt)
+//     next_time = next_time + min_dt
+
+//     --rest of function here
+//     end
+
+//     function love.draw()
+//     --rest of function here
+
+//     local cur_time = love.timer.getTime()
+//     if next_time <= cur_time then
+//                   next_time = cur_time
+//                   return
+//                   end
+//     love.timer.sleep(next_time - cur_time)
+//     end
