3e47039dc3fba38596f149d20cf812ca7d35ed7e
Simon Heath <icefoxen@gmail.com>
2016-12-09 10:43:33 -0500

    Added mouse events

--- src/event.rs
+++ src/event.rs
@@ -1,5 +1,8 @@
 //! Currently only contains types for keyboard keycodes and modifier keys.
+//! As well as mouse states.
 
 
 pub use sdl2::keyboard::Keycode;
 pub use sdl2::keyboard::Mod;
+pub use sdl2::mouse::Mouse;
+pub use sdl2::mouse::MouseState;
