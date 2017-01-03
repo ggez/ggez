a2e66da466b9425f2ad1da24f8c2b180385bc7e8
Simon Heath <icefoxen@gmail.com>
2017-01-03 09:39:24 -0500

    Working on crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+//! # ggez
+//!
+//! ggez is a framework for creating 2D video games.  Its API is
+//! heavily based on the Love2D game engine, though inevitably
+//! with some differences due to the fact that Rust is not Lua.
+//!
+//! ggez consists of three main parts: A `Context` object which contains
+//! all the state requierd to interface with the computer's hardware,
+//! a `GameState` trait that the user implements to register callbacks
+//! for events, and various sub-modules such as `graphics` and `audio`
+//! that provide the functionality to actually get stuff done.
+//!
+//! Note that ggez isn't intended to be everything to everyone; it is
+//! deliberately opinionated and tries to provide a good basic framework
+//! for getting stuff done rather than having a million options for everything
+//! imaginable.
+//!
+//! # Implementation notes
+//!
+//! ggez is a fairly thin wrapper around SDL2 and a few other
+//! libraries, which does influence some of the API and impose some
+//! restrictions.  For example, thread safety.
+//!
+//! ## Thread safety
+//!
+//! SDL2 is generally speaking NOT thread-safe.  It uses lots of globals
+//! internally, and just isn't designed with thread safety in mind.  This
+//! isn't generally a huge restriction in C code, but in Rust the practical
+//! result is that none of the types derived from SDL2 are `Send` or `Sync`,
+//! such as the `ggez::graphics::Image` type.
+
 extern crate sdl2;
 extern crate sdl2_image;
 extern crate sdl2_mixer;
