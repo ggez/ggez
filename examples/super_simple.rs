a60463811696434ed888b00cb8baa34496c0cca5
Sven-Hendrik Haase <svenstaro@gmail.com>
2017-05-01 13:12:55 +0200

    Add a super simple example with a moving rectangle

---
+++ examples/super_simple.rs
@@ -0,0 +1,39 @@
+extern crate ggez;
+use ggez::conf;
+use ggez::event;
+use ggez::{GameResult, Context};
+use ggez::graphics;
+use ggez::graphics::{Color, DrawMode, Point};
+use std::time::Duration;
+
+struct MainState {
+    pos_x: f32,
+}
+
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let s = MainState { pos_x: 0.0 };
+        Ok(s)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context, _dt: Duration) -> GameResult<()> {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx);
+        graphics::circle(ctx, DrawMode::Fill, Point { x: self.pos_x, y: 380.0 }, 100.0, 32)?;
+        graphics::present(ctx);
+        Ok(())
+    }
+}
+
+pub fn main() {
+    let c = conf::Conf::new();
+    let ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
+    let state = &mut MainState::new(ctx).unwrap();
+    event::run(ctx, state).unwrap();
+}
