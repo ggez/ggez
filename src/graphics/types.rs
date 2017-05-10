311cee3134e2b25c2acc5a5eb916f60a500d0c1b
Simon Heath <icefoxen@gmail.com>
2017-05-10 13:06:15 -0400

    we really need real color lists

--- src/graphics/types.rs
+++ src/graphics/types.rs
@@ -101,6 +101,14 @@ pub const WHITE: Color = Color {
     a: 1.0,
 };
 
+
+pub const BLACK: Color = Color {
+    r: 1.0,
+    g: 1.0,
+    b: 1.0,
+    a: 1.0,
+};
+
 impl Color {
     pub fn new(r: f32, g: f32, b: f32, a: f32) -> Self {
         Color {
