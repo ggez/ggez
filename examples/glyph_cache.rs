e8fa88667e09829904709d964f6663a7cb0b4541
dettalant <lilac@dettalant.com>
2018-03-31 19:03:39 +0900

    rustfmt glyph_cache.rs

--- examples/glyph_cache.rs
+++ examples/glyph_cache.rs
@@ -1,14 +1,14 @@
 //! How to glyph cache text render in ggez.
-extern crate ggez;
-extern crate gfx_glyph;
 extern crate gfx_device_gl;
+extern crate gfx_glyph;
+extern crate ggez;
 
 use ggez::conf;
-use ggez::event::{ self, EventHandler };
-use ggez::{ Context, ContextBuilder, GameResult };
+use ggez::event::{self, EventHandler};
 use ggez::graphics;
+use ggez::{Context, ContextBuilder, GameResult};
 
-use gfx_glyph::{ GlyphBrush, GlyphBrushBuilder, Section };
+use gfx_glyph::{GlyphBrush, GlyphBrushBuilder, Section};
 
 struct MainState {
     glyph_brush: GlyphBrush<'static, gfx_device_gl::Resources, gfx_device_gl::Factory>,
@@ -26,8 +26,10 @@ impl MainState {
         let font = include_bytes!("../resources/DejaVuSerif.ttf");
 
         // Set window size.
-        let (window_w, window_h) = (ctx.conf.window_mode.width as f32,
-                                    ctx.conf.window_mode.height as f32);
+        let (window_w, window_h) = (
+            ctx.conf.window_mode.width as f32,
+            ctx.conf.window_mode.height as f32,
+        );
 
         // get gfx-rs Factory object.
         let factory = graphics::get_factory(ctx);
@@ -48,8 +50,7 @@ impl MainState {
 }
 
 impl EventHandler for MainState {
-    fn update(&mut self,
-              ctx: &mut Context) -> GameResult<()> {
+    fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
         self.frames += 1;
         if self.frames % 100 == 0 {
             println!("FPS: {}", ggez::timer::get_fps(ctx));
@@ -71,15 +72,11 @@ impl EventHandler for MainState {
         Ok(())
     }
 
-    fn draw(&mut self,
-            ctx: &mut Context) -> GameResult<()> {
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         // Do gfx-rs drawing
         {
-            let (_factory,
-                 _device,
-                 mut encoder,
-                 depthview,
-                 colorview) = graphics::get_gfx_objects(ctx);
+            let (_factory, _device, mut encoder, depthview, colorview) =
+                graphics::get_gfx_objects(ctx);
 
             // Almost the same as ggez::graphics::clear()
             encoder.clear(&colorview, [0.03, 0.03, 0.03, 1.0]);
@@ -95,8 +92,7 @@ impl EventHandler for MainState {
             // Tutorial message
             self.glyph_brush.queue(Section {
                 screen_position: (0.0, self.window_h * 0.08),
-                bounds: (self.window_w,
-                         self.window_h),
+                bounds: (self.window_w, self.window_h),
                 text: "Let's resize this window!",
                 scale: gfx_glyph::Scale::uniform(42.0),
                 color: [0.9, 0.3, 0.3, 1.0],
@@ -106,8 +102,7 @@ impl EventHandler for MainState {
             // Wrap text
             self.glyph_brush.queue(Section {
                 screen_position: (0.0, self.window_h * 0.3),
-                bounds: (self.window_w,
-                         self.window_h),
+                bounds: (self.window_w, self.window_h),
                 text: "The quick brown fox jumps over the lazy dog",
                 scale: gfx_glyph::Scale::uniform(36.0),
                 color: [0.3, 0.3, 0.9, 1.0],
@@ -117,8 +112,7 @@ impl EventHandler for MainState {
             // Like a typewriter
             self.glyph_brush.queue(Section {
                 screen_position: (0.0, self.window_h * 0.6),
-                bounds: (self.window_w,
-                         self.window_h),
+                bounds: (self.window_w, self.window_h),
                 text: &self.text_base,
                 scale: gfx_glyph::Scale::uniform(36.0),
                 color: [0.3, 0.9, 0.3, 1.0],
@@ -137,10 +131,7 @@ impl EventHandler for MainState {
         Ok(())
     }
 
-    fn resize_event(&mut self,
-                    _ctx: &mut Context,
-                    width: u32,
-                    height: u32) {
+    fn resize_event(&mut self, _ctx: &mut Context, width: u32, height: u32) {
         println!("Resized screen to {}, {}", width, height);
         // reset window size
         self.window_w = width as f32;
@@ -171,11 +162,12 @@ fn unused_dir_remove(ctx: &mut Context) -> GameResult<()> {
 
 pub fn main() {
     let cb = ContextBuilder::new("glyph_cache_example", "ggez")
-        .window_setup(conf::WindowSetup::default()
-            .title("glyph cache example")
-            .resizable(true))
-        .window_mode(conf::WindowMode::default()
-            .dimensions(512, 512));
+        .window_setup(
+            conf::WindowSetup::default()
+                .title("glyph cache example")
+                .resizable(true),
+        )
+        .window_mode(conf::WindowMode::default().dimensions(512, 512));
 
     let ctx = &mut cb.build().unwrap();
 
