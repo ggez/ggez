1c62af4b26460f4aeb54d12e39f65f81b91a5708
Martin Billinger <flkazemakase@gmail.com>
2018-07-29 19:01:24 +0200

    No longer hang on to unwanted sources

--- examples/imageview.rs
+++ examples/imageview.rs
@@ -19,9 +19,6 @@ struct MainState {
     image: graphics::Image,
     text: graphics::Text,
     pixel_sized_text: graphics::Text,
-    // Not actually dead, see BUGGO below
-    #[allow(dead_code)]
-    sound: audio::Source,
 }
 
 impl MainState {
@@ -59,7 +56,8 @@ impl MainState {
         let pixel_sized_text =
             graphics::Text::new(("This text is 32 pixels high", font, 32.0));
 
-        let _ = sound.play();
+        // "detached" sounds keep playing even after they are dropped
+        let _ = sound.play_detached();
 
         let s = MainState {
             a: 0,
@@ -67,12 +65,6 @@ impl MainState {
             image,
             text,
             pixel_sized_text,
-            // BUGGO: We never use sound again,
-            // but we have to hang on to it, Or Else!
-            // The optimizer will decide we don't need it
-            // since play() has "no side effects" and free it.
-            // Or something.
-            sound,
         };
 
         Ok(s)
