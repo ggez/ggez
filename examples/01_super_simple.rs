d208543085e90d927657b42022297ae8ab474ebe
Simon Heath <icefox@dreamquest.io>
2019-07-16 14:49:43 -0400

    Consistentify 01_super_simple example

--- examples/01_super_simple.rs
+++ examples/01_super_simple.rs
@@ -29,12 +29,12 @@ impl event::EventHandler for MainState {
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            na::Point2::new(self.pos_x, 380.0),
+            na::Point2::new(0.0, 0.0),
             100.0,
             2.0,
             graphics::WHITE,
         )?;
-        graphics::draw(ctx, &circle, (na::Point2::new(0.0, 0.0),))?;
+        graphics::draw(ctx, &circle, (na::Point2::new(self.pos_x, 380.0),))?;
 
         graphics::present(ctx)?;
         Ok(())
