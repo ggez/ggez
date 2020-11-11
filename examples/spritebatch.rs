838e6cdecbabb4c1816c44709e92831617cbeb8e
Simon Heath <icefox@dreamquest.io>
2020-11-11 12:17:32 -0500

    Minor version bumps

--- examples/spritebatch.rs
+++ examples/spritebatch.rs
@@ -99,8 +99,8 @@ pub fn main() -> GameResult {
     };
 
     let cb = ggez::ContextBuilder::new("spritebatch", "ggez").add_resource_path(resource_dir);
-    let (ctx, event_loop) = &mut cb.build()?;
+    let (mut ctx, event_loop) = cb.build()?;
 
-    let state = &mut MainState::new(ctx)?;
+    let state = MainState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
 }
