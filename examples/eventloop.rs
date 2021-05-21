e71e0eecf4926d0add7991248a7c19df9da08966
PSteinhaus <philipp_steinhaus@web.de>
2021-05-21 13:38:30 +0200

    fixed eventloop example by updating it to actually work like the current implementation of ggez::event::run()

--- examples/eventloop.rs
+++ examples/eventloop.rs
@@ -12,63 +12,71 @@ use ggez::event;
 use ggez::event::winit_event::{Event, KeyboardInput, WindowEvent};
 use ggez::graphics::{self, Color, DrawMode};
 use ggez::GameResult;
+use winit::event_loop::ControlFlow;
 
 pub fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("eventloop", "ggez");
-    let (ref mut ctx, mut events_loop) = cb.build()?;
+    let (mut ctx, events_loop) = cb.build()?;
 
     let mut position: f32 = 1.0;
 
-    // This is also used in the loop inside `::run()` - it can be flipped off with `event::quit()`
-    while ctx.continuing {
-        // Tell the timer stuff a frame has happened.
-        // Without this the FPS timer functions and such won't work.
-        ctx.timer_context.tick();
-        // Handle events. Refer to `winit` docs for more information.
-        use winit::platform::run_return::EventLoopExtRunReturn;
-        events_loop.run_return(|event, _window_target, control_flow| {
-            // This tells `ggez` to update it's internal states, should the event require that.
-            // These include cursor position, view updating on resize, etc.
-            event::process_event(ctx, &event);
-            match event {
-                Event::WindowEvent { event, .. } => match event {
-                    WindowEvent::CloseRequested => event::quit(ctx),
-                    WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                virtual_keycode: Some(keycode),
-                                ..
-                            },
-                        ..
-                    } => {
-                        if let event::KeyCode::Escape = keycode {
-                            *control_flow = winit::event_loop::ControlFlow::Exit
-                        }
+    // Handle events. Refer to `winit` docs for more information.
+    events_loop.run(move |event, _window_target, control_flow| {
+        if !ctx.continuing {
+            *control_flow = ControlFlow::Exit;
+            return;
+        }
+
+        *control_flow = ControlFlow::Poll;
+
+        let ctx = &mut ctx;
+
+        // This tells `ggez` to update it's internal states, should the event require that.
+        // These include cursor position, view updating on resize, etc.
+        event::process_event(ctx, &event);
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => event::quit(ctx),
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(keycode),
+                            ..
+                        },
+                    ..
+                } => {
+                    if let event::KeyCode::Escape = keycode {
+                        *control_flow = winit::event_loop::ControlFlow::Exit
                     }
-                    // `CloseRequested` and `KeyboardInput` events won't appear here.
-                    x => println!("Other window event fired: {:?}", x),
-                },
+                }
+                // `CloseRequested` and `KeyboardInput` events won't appear here.
+                x => println!("Other window event fired: {:?}", x),
+            },
+            Event::MainEventsCleared => {
+                // Tell the timer stuff a frame has happened.
+                // Without this the FPS timer functions and such won't work.
+                ctx.timer_context.tick();
 
-                x => println!("Device event fired: {:?}", x),
+                // Update
+                position += 1.0;
+
+                // Draw
+                graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+                let circle = graphics::Mesh::new_circle(
+                    ctx,
+                    DrawMode::fill(),
+                    glam::Vec2::new(0.0, 0.0),
+                    100.0,
+                    2.0,
+                    Color::WHITE,
+                )
+                .unwrap();
+                graphics::draw(ctx, &circle, (glam::Vec2::new(position, 380.0),)).unwrap();
+                graphics::present(ctx).unwrap();
+                ggez::timer::yield_now();
             }
-        });
 
-        // Update
-        position += 1.0;
-
-        // Draw
-        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            DrawMode::fill(),
-            glam::Vec2::new(0.0, 0.0),
-            100.0,
-            2.0,
-            Color::WHITE,
-        )?;
-        graphics::draw(ctx, &circle, (glam::Vec2::new(position, 380.0),))?;
-        graphics::present(ctx)?;
-        ggez::timer::yield_now();
-    }
-    Ok(())
+            x => println!("Device event fired: {:?}", x),
+        }
+    });
 }
