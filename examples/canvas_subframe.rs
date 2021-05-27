f534ba580411809f74cd3aff39c6bdb11b278be6
PSteinhaus <philipp_steinhaus@web.de>
2021-05-27 20:24:40 +0200

    rustfmt

--- examples/canvas_subframe.rs
+++ examples/canvas_subframe.rs
@@ -99,8 +99,7 @@ impl event::EventHandler for MainState {
         if self.draw_pt.x + (w as f32 / 2.0) > (w as f32) || self.draw_pt.x < 0.0 {
             self.draw_vec.x *= -1.0;
         }
-        if self.draw_pt.y + (h as f32 / 2.0) > (h as f32) || self.draw_pt.y < 0.0
-        {
+        if self.draw_pt.y + (h as f32 / 2.0) > (h as f32) || self.draw_pt.y < 0.0 {
             self.draw_vec.y *= -1.0;
         }
         self.draw_pt += self.draw_vec;
