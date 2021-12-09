c1638162f2caa0030baea3b60bcaa372bc2dd8ad
Liam Borella <ulfrthewolf@gmail.com>
2021-12-10 03:20:20 +1000

    Ran rustfmt

--- src/event.rs
+++ src/event.rs
@@ -523,7 +523,7 @@ where
                 // necessary because it's calculated cumulatively each cycle
                 ctx.mouse_context.reset_delta();
 
-				// Copy the state of the keyboard into the KeyboardContext
+                // Copy the state of the keyboard into the KeyboardContext
                 // and the mouse into the MouseContext
                 ctx.keyboard_context.save_keyboard_state();
                 ctx.mouse_context.save_mouse_state();
